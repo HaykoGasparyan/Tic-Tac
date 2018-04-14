6a52849882a9c031687c88aee289b2b61e470ccc
Armenian Academy <armeniancodeacademy@davos-macbook-air.local>
2018-04-15 01:18:35 +0400

    Cleared code, added some comments

--- Tic-Tac/ViewController.swift
+++ Tic-Tac/ViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 import AVFoundation
 
 extension UIView {
+    //Rotates view by "angle" parameter degree
     func rotate(angle: CGFloat) {
         let radians = angle / 180.0 * CGFloat(Double.pi)
         let rotation = self.transform.rotated(by: radians)
         self.transform = rotation
     }
     
+    //Sets anchor point for rotation to any point
+    //Got from google
+    //anchorPoint - coordinate of point, which wanted to be set as anchor point (0-1).
     func setAnchorPoint(anchorPoint: CGPoint) {
-        
         var newPoint = CGPoint(x: self.bounds.size.width * anchorPoint.x, y: self.bounds.size.height * anchorPoint.y)
         var oldPoint = CGPoint(x: self.bounds.size.width * self.layer.anchorPoint.x, y: self.bounds.size.height * self.layer.anchorPoint.y)
         
@@ -42,34 +45,31 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     
     @IBOutlet weak var stackView: UIStackView!
     
-    var BPM: Double = 110
-    var thirdHelperFlag = true
-    var secondHelperFlag = true
-    var helperFlag = true
+    var BPM: Double = 110       //BPM -> Beats per minute
+    var toRight = true
+    var isPuased = true
     var isRunning = false
     var rectangle: UIView!
     let trapezium = CAShapeLayer()
     var pauseResumeButton: UIButton!
     var speedButton: UIButton!
-    var player1 = AVAudioPlayer()
-    var player2 = AVAudioPlayer()
-    var count = 0
+    var tac = AVAudioPlayer()
+    var tic = AVAudioPlayer()
+    var ticCount = 0
     let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.light)
     var blurEffectView: UIVisualEffectView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        player1.prepareToPlay()
-//        player2.prepareToPlay()
         stackView.alpha = 0
         drawObjects()
         do {
-            let audioPlayer1 = Bundle.main.path(forResource: "Metronome", ofType: "wav")
-            let audioPlayer2 = Bundle.main.path(forResource: "MetronomeUp", ofType: "wav")
-            try player1 = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPlayer1!) as URL)
-            try player2 = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPlayer2!) as URL)
-            player1.prepareToPlay()
-            player2.prepareToPlay()
+            let tacAudioPlayer = Bundle.main.path(forResource: "Metronome", ofType: "wav")
+            let ticAudioPlayer = Bundle.main.path(forResource: "MetronomeUp", ofType: "wav")
+            try tac = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: tacAudioPlayer!) as URL)
+            try tic = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: ticAudioPlayer!) as URL)
+            tac.prepareToPlay()
+            tic.prepareToPlay()
         } catch {
             
         }
@@ -82,65 +82,68 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         }
     }
     
-    @IBAction func speedChosen(_ sender: UIButton) {
+    @IBAction func chooseSpeed(_ sender: UIButton) {
+        let tag = sender.tag
         BPM = Double(sender.tag)
-        if sender.tag == 70 {
+        if tag == 70 {
             speedButton.setTitle("70", for: .normal)
         }
-        if sender.tag == 90 {
+        if tag == 90 {
             speedButton.setTitle("90", for: .normal)
         }
-        if sender.tag == 110 {
+        if tag == 110 {
             speedButton.setTitle("110", for: .normal)
         }
-        if sender.tag == 130 {
+        if tag == 130 {
             speedButton.setTitle("130", for: .normal)
         }
         stackView.alpha = 0
         blurEffectView.removeFromSuperview()
     }
     
-     func pressedPlay() {
-        if self.thirdHelperFlag {
+    //Plays animation
+    //This function is also called in completion of animation(recursia) to animate to the oppossite side
+    func playAnimation() {
+        if !self.isPuased {
             UIView.animate(withDuration: 60/BPM, animations: {
                 self.isRunning = true
-                if self.secondHelperFlag == true {
+                if self.toRight == true {
                     self.rectangle.rotate(angle: 90)
                 } else {
                     self.rectangle.rotate(angle: -90)
                 }
             }) { (finished) in
-                if self.count == 3 {
-                    self.player1.play()
-                    self.count = 0
+                if self.ticCount == 3 {
+                    self.tac.play()
+                    self.ticCount = 0
                 } else {
-                    self.player2.play()
-                    self.count += 1
+                    self.tic.play()
+                    self.ticCount += 1
                 }
-                self.secondHelperFlag = !self.secondHelperFlag
-                if self.helperFlag {
+                self.toRight = !self.toRight
+                if self.isPuased {
                     self.isRunning = false
                 }
-                self.pressedPlay()
+                self.playAnimation()
             }
         }
     }
     
-    @objc func pressedButton() {
-        if helperFlag {
-            thirdHelperFlag = true
+    //This method is called when pausePlayButtonPressed.
+    @objc func pausePlayButtonPressed() {
+        if isPuased {
             pauseResumeButton.setImage(UIImage(named: "pause"), for: .normal)
-            helperFlag = false
+            isPuased = false
             if !isRunning {
-                pressedPlay()
+                playAnimation()
             }
         } else {
             pauseResumeButton.setImage(UIImage(named: "resume"), for: .normal)
-            helperFlag = true
-            thirdHelperFlag = false
+            isPuased = true
         }
     }
     
+    //adds blure effect and stackView comes visible
     @objc func speedButtonPressed() {
         blurEffectView = UIVisualEffectView(effect: blurEffect)
         blurEffectView.frame = view.bounds
@@ -151,12 +154,13 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         stackView.alpha = 1
     }
     
+    //drawing trapezium,arrow for tic-tac and two buttons.
     func drawObjects() {
         let screenSize = UIScreen.main.bounds
         let width = screenSize.width
         let height = screenSize.height
         
-        let trapeziumPath = UIBezierPath()
+        let trapeziumPath = UIBezierPath()      //Researched in google (UIBezierPath())
         trapeziumPath.move(to: CGPoint(x: width * 1/4, y: height * 1/8))
         trapeziumPath.addLine(to: CGPoint(x: width * 3/4, y: height * 1/8))
         trapeziumPath.addLine(to: CGPoint(x: width * 7/8, y: height * 3/8))
@@ -176,7 +180,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         let buttonRectFrame = CGRect(x: width * 3/8, y: height * 5/8, width: width * 1/4, height: width * 1/4)
         pauseResumeButton = UIButton(frame: buttonRectFrame)
         pauseResumeButton.setImage(UIImage(named: "resume"), for: .normal)
-        pauseResumeButton.addTarget(self, action: #selector(pressedButton), for: .touchUpInside)
+        pauseResumeButton.addTarget(self, action: #selector(pausePlayButtonPressed), for: .touchUpInside)
         self.view.addSubview(pauseResumeButton)
         
         let buttonRect = CGRect(x: width * 1/2 - width * 3/16 , y: height * 13/16, width: width * 3/8, height: height * 1/16)
